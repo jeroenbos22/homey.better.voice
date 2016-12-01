e6bb5b83d00cd45d3b96eaaf4016eaae442e37c9
jeroenbos22 <jeroen@MacBook-Pro-van-Het-2.local>
2016-12-02 00:38:08 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Better Voice
+        
+This app let's you correct words that Homey understood wrong using voice.
+
+Example, sometimes when you say "Lights on" and Homey understands "Nights on". Using this app you can auto-correct that and re-emulate the complete sentence in a flow (see image below). You can even ask Homey for a confirmation if you don't want it to be auto-executed.
+
+![](http://i.imgur.com/eLmhQsr.png)
+
+
+V0.0.1:
+
+* First version
---
+++ app.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var tokens, trigger, replaced, corrections = {};
+
+function init() {
+    corrections = Homey.manager('settings').get('corrections');
+    
+    Object.keys(corrections).forEach(function (key) {
+        Homey.manager('speech-input').addTrigger({
+            "id": "better-voice-"+key,
+            "importance": 1,
+            "triggers": {
+                "en": [ key ],
+                "nl": [ key ]
+            }
+        });
+        Homey.log("Trigger added for "+key);
+     });
+	
+	Homey.log("Homey Better Voice app ready!");
+	
+}
+
+Homey.manager('speech-input').on('speech', function(speech){
+	trigger = speech.triggers[0].text;
+	Homey.log('Trigger '+trigger+' detected');
+	if (typeof trigger === 'string' && (trigger in corrections)) {
+		Homey.log('Trigger '+trigger+' executed');
+        
+        replaced = speech.transcript.replace(trigger, corrections[trigger]);
+        tokens = {'corrected_sentence': replaced, 'corrected_word': corrections[trigger] };
+        
+        Homey.manager('flow').trigger('better_voice_trigger', tokens, null, function(err, result){
+            if( err ) return Homey.error(err);
+        });
+    }
+});
+
+module.exports.init = init;
