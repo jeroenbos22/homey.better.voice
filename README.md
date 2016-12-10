b1186e5cf9754aeff14056182762f3e1265a56ad
jeroenbos22 <jeroen@MacBook-Pro-van-Het-2.local>
2016-12-10 21:46:38 +0100

    V0.05 - Dutch translation + combined speech-commands

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # Better Voice
         
-This app let's you correct words that Homey understood wrong using voice.
+This app let's you correct words that Homey understood wrong using voice and you can combine speech command with "AND" (or "EN" for dutch) in your sentence.
 
 Example, sometimes when you say "Lights on" and Homey understands "Nights on". Using this app you can auto-correct that and re-emulate the complete sentence in a flow (see image below). You can even ask Homey for a confirmation if you don't want it to be auto-executed.
 
 ![](http://i.imgur.com/eLmhQsr.png)
 
+In order to let the combined speech-commands work you need to have a flow like:
+![](http://i.imgur.com/QCt25SE.png)
+
+
+V0.0.5:
+
+* Added dutch translation and combined speech-commands
 
 V0.0.4:
 
--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 "use strict";
 
 var tokens, trigger, replaced, corrections = {};
+var combine_array = [];
 
 function setupTriggers() {
+    if (Homey.manager('settings').get('combinations') == 1) {
+        Homey.log("Use combinations, add trigger");
+        Homey.manager('speech-input').addTrigger({
+            "id": "better-voice-combinations-trigger",
+            "importance": 1,
+            "triggers": {
+                "en": ["and"],
+                "nl": ["en"]
+            }
+        });
+    } else {
+        Homey.manager('speech-input').removeTrigger('better-voice-combinations-trigger');
+        Homey.log("Not use combinations, remove trigger");
+    }
+    
     corrections = Homey.manager('settings').get('corrections');
-
     if (typeof corrections === 'object') {
         Object.keys(corrections).forEach(function (key) {
             Homey.manager('speech-input').addTrigger({
@@ -30,7 +45,6 @@ Homey.manager('settings').on('set', function (setting) {
     if (setting !== 'corrections') {
         return;
     }
-
     setupTriggers();
 });
 
@@ -46,6 +60,30 @@ Homey.manager('speech-input').on('speech', function (speech) {
         Homey.manager('flow').trigger('better_voice_trigger', tokens, {session: speech.session}, function (err, result) {
             if (err) return Homey.error(err);
         });
+    } else {
+        replaced = speech.transcript;
+    }
+    
+    if (typeof trigger === 'string' && (trigger === "and" || trigger === "en")) {
+        if (Homey.manager('i18n').getLanguage() == 'nl') {
+            combine_array = replaced.split(" en ",3); // If ducth find " en "
+        } else {
+            combine_array = replaced.split(" and ",3); // If other find " and "
+        }
+        
+        for (var key in combine_array) {
+            combine_array[key] = combine_array[key].replace('and',' ').replace('en',' ').trim();
+        }
+        
+        tokens = {'combine_1': combine_array[0], 'combine_2': combine_array[1], 'combine_3': combine_array[2]};
+        
+        Homey.log(tokens);
+
+        Homey.manager('flow').trigger('better_voice_combine', tokens, {session: speech.session}, function (err, result) {
+            if (err) return Homey.error(err);
+        });
+        
+        
     }
 });
 
